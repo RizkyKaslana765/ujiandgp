0b2afec7460f3c6fc9890eaeb20b5ab9952ce9d2
RizkyKaslana765 <119464486+RizkyKaslana765@users.noreply.github.com>
2022-12-09 09:35:57 +0700

    Update timer2.js

--- timer2.js
+++ timer2.js
@@ -1,4 +1,4 @@
-var countdown = new Date("Dec 08, 2022 10:30:00").getTime();
+var countdown = new Date("Dec 09, 2022 13:00:00").getTime();
 var myfunc = setInterval(function() {
 
     var now = new Date().getTime();
