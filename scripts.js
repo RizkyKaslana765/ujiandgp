ae2b0591a3a7d557a250e5cf3fe61f090e0ced09
RizkyKaslana765 <119464486+RizkyKaslana765@users.noreply.github.com>
2022-12-09 09:58:32 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,22 +2,22 @@ function gachaSoal(){
     let gacha = Math.floor(Math.random() * (4 - 1)) + 1;
     console.log(gacha);
     if (gacha == 1){
-        document.getElementById("gotSoalPink").innerHTML = " <div class='menu-pink round-corner margin-center'> <h3 class='center'> Kamu mendapatkan soal Pink!!  </h3> </div>"
+        document.getElementById("gotSoalPink").innerHTML = " <div class='menu-blue round-corner margin-center'> <h3 class='center'> Waitt </h3> </div>"
         console.log("Soal Pink");
         setTimeout(function() { 
             console.log("wleee"); 
-            document.body.style.backgroundColor = "#EFBAD8";
-            document.getElementById("topbar").style.backgroundColor = "#BB5C94";
+            document.body.style.backgroundColor = "#BCCBDA";
+            document.getElementById("topbar").style.backgroundColor = "#215182";
             document.getElementById("soalPink").style.display = "block";
             document.getElementById("pageOne").style.display = "none";
         },3000);
     } else if (gacha == 3 ){
-        document.getElementById("gotSoalGreen").innerHTML = " <div class='menu-green round-corner margin-center'> <h3 class='center'> Kamu mendapatkan soal Hijau!!  </h3> </div>"
+        document.getElementById("gotSoalGreen").innerHTML = " <div class='menu-pink round-corner margin-center'> <h3 class='center'> Waitt  </h3> </div>"
         console.log("Soal Green");
         setTimeout(function() { 
             console.log("wleee2"); 
-            document.body.style.backgroundColor = "#BCD5B9";
-            document.getElementById("topbar").style.backgroundColor = "#207315";
+            document.body.style.backgroundColor = "#FFE4AF";
+            document.getElementById("topbar").style.backgroundColor = "#FFBC36";
             document.getElementById("soalGreen").style.display = "block";
             document.getElementById("pageOne").style.display = "none";
         },3000);
